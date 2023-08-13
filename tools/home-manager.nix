1bc04f0b8c475e32493cad2f3d1cdbeda820ae6d
martin-bucinskas <martin@martinb.dev>
2023-08-13 15:11:55 +0100

    Aliasing vim to nvim using session variables

--- tools/home-manager.nix
+++ tools/home-manager.nix
@@ -3,4 +3,12 @@
     home.username = "martini";
     home.homeDirectory = "/home/martini";
     programs.home-manager.enable = true;
+
+    home.sessionVariables = {
+        BASH_ENV = "${pkgs.bash}/etc/profile.d/alias-vim-to-nvim.sh";
+    };
+
+    environment.etc."profile.d/alias-vim-to-nvim.sh".text = ''
+        alias vim='nvim'
+    '';
 }
