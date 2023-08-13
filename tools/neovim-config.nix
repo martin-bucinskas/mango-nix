417df42512f7c9d0d9b3196e48741d8f961f3782
martin-bucinskas <martin@martinb.dev>
2023-08-13 15:45:53 +0100

    Refactoring neovim config

--- tools/neovim-config.nix
+++ tools/neovim-config.nix
@@ -1,17 +1,21 @@
-{ pkgs, lib, ... }: {
+{ pkgs ? import <nixpkgs> {}, lib, ... }: 
+let
+    myVimPackages = with pkgs.vimPlugins; [
+        "vim-airline"
+        "williamboman/mason.nvim"
+        "williamboman/mason-lspconfig.nvim"
+    ];
 
     home.file.".config/nvim/init.lua" = {
         source = ./neovim/init.lua;
         mode = "0644";
     };
-
+in {
     programs.neovim = {
         enable = true;
-
-        plugins = with pkgs.vimPlugins; [
-            vim-airline
-            "williamboman/mason.nvim"
-            "williamboman/mason-lspconfig.nvim"
-        ];
+        
+        packages.myVimPackages = {
+            start = myVimPackages;
+        };
     };
 }
