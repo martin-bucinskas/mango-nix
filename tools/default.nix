d8e46d6389f6755c6191b187e9a010b027c70a5a
martin-bucinskas <martin@martinb.dev>
2023-08-14 18:50:00 +0100

    removing nixvim

--- tools/default.nix
+++ tools/default.nix
@@ -1,9 +1,10 @@
-{ config, lib, pkgs, ... }: {
+{ config, lib, pkgs, nixvim, ... }: {
     imports = [
         ./nixvim-config.nix
         ./home-manager.nix
         ./git.nix
         ./bash.nix
+        ./neovim-config.nix
         ./languages
     ];
 
